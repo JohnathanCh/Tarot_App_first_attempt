bbfbc335ab8bf21b95c015362cd805d2d6afc01c
github username <github email address>
2018-11-30 16:22:16 -0500

    backend

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,31 @@
+module api
+    module v1
+        class UsersController < ApplicationController
+
+            def index
+                @users = User.all
+                render json: @users
+            end
+
+            def show
+        
+                @user = User.all.find(params[:id])
+                @readings = @user.readings
+        
+                @userReadings = []
+
+                @readings.each do |reading|
+                    @userReadings.push({readingName: reading.reading_title, readingCards: [*reading.cards]})
+        
+                end
+        
+                render json: @userReadings
+            end
+
+            def create
+                @user
+            end
+
+        end
+    end
+end

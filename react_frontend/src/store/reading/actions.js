5984f14d7adc29681a1f11583101e39fc660ad1c
github username <github email address>
2018-11-30 12:54:37 -0500

    import actions and reducers.

---
+++ react_frontend/src/store/reading/actions.js
@@ -0,0 +1,53 @@
+// /*--------------- Action Types ---------------*/
+// export const CREATE_READING = "CREATE_READING"
+// export const READING_CARDS = "READING_CARDS"
+
+
+// /*--------------- Action Creators---------------*/
+// export const createReadingAction = (reading) => {
+//     return({
+//         type: CREATE_READING,
+//         payload: reading 
+//     })
+// }
+
+// export const readingCardsAction = (cards) => {
+//     return({
+//         type: READING_CARDS,
+//         payload: cards
+//     })
+// }
+
+
+// /*--------------- Thunk Creator ---------------*/
+
+
+// //I think I have to send a user as well to this.
+// export const createReading = (readingCards, user) => {
+//     return function thunk(dispatch) {
+        
+//         const options = {
+//             method: "POST",
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Accept': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                user: {
+//                     ...user
+//                 },
+//                readingCards: [
+//                     ...readingCards
+//                ]
+//             })
+//         }
+
+//         fetch('http://localhost:3000/readings', options)
+//         .then(resp => resp.json())
+//         .then(reading => {console.log("This is a popsicle stick I'm useing to keep the castle standing")})
+//     }
+// }
+
+// // fetch('http://localhost:3000/readings', options)
+// // .then(resp => resp.json())
+// // .then(reading => dispatch(createReadingAction(reading)))
---
+++ react_frontend/src/store/reading/reducer.js
@@ -0,0 +1,25 @@
+// import * as actions from './actions'
+
+// const initialState = {
+//     cardList: [],
+//     user: {},
+//     readingCards: []
+// }
+
+
+//  const readingReducer = (state = initialState, action) => {
+//      console.log("In the Reading Reducer", state);
+     
+//     switch (action.type) {
+//         case actions.CREATE_READING:
+//         return {
+//             ...state,
+//             readingCards: [...action.payload]
+//         }
+            
+//         default:
+//             return state
+//     }
+// }
+
+// export default readingReducer;

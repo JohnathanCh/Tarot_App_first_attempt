00cb6acf5b951ffe4df3d979b71f126ac4640960
github username <github email address>
2019-01-01 21:42:35 -0500

    added signup to welcome page

--- frontend_react/src/components/Welcome.js
+++ frontend_react/src/components/Welcome.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Route } from 'react-router'
+import Signup from '../login_signup/Signup';
+
 
 export default class Welcome extends React.Component {
 
@@ -7,6 +10,8 @@ export default class Welcome extends React.Component {
             <div>
 
             <img src="http://media.beliefnet.com/~/media/photos-with-attribution/spiritual/tarotcardscreditShutterstockcom.jpg?as=1" className="bg"/>
+
+            <Route path="/" component={Signup}/>
             
             </div>
         )

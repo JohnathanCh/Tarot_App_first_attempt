95637a36bff98632ba828acd9ca571218f46c461
github username <github email address>
2019-01-01 23:04:44 -0500

    Make login welcome page better

--- frontend_react/src/components/Welcome.js
+++ frontend_react/src/components/Welcome.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router'
 import Signup from '../login_signup/Signup';
+import Login from '../login_signup/Login';
 
 
 export default class Welcome extends React.Component {
@@ -9,6 +10,9 @@ export default class Welcome extends React.Component {
         return (
             <div>
 
+                <Route path="/" component={Login} />
+                <Route path="/signup" component={Signup} />
+ 
             <img src="http://media.beliefnet.com/~/media/photos-with-attribution/spiritual/tarotcardscreditShutterstockcom.jpg?as=1" className="bg"/>
 
             <Route path="/" component={Signup}/>

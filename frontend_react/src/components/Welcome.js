c682de3be9d5f903e62343eec1383ad6c40ef602
github username <github email address>
2019-01-03 14:32:23 -0500

    little commit

--- frontend_react/src/components/Welcome.js
+++ frontend_react/src/components/Welcome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router'
+import { Switch, Route } from 'react-router'
 import Signup from '../login_signup/Signup';
 import Login from '../login_signup/Login';
 
@@ -9,13 +9,12 @@ export default class Welcome extends React.Component {
     render() {
         return (
             <div>
-
-                <Route path="/" component={Login} />
-                <Route path="/signup" component={Signup} />
+                <Switch >
+                    <Route exact path="/" component={Login} />
+                    <Route exact path="/signup" component={Signup} />
+                </Switch>
  
             <img src="http://media.beliefnet.com/~/media/photos-with-attribution/spiritual/tarotcardscreditShutterstockcom.jpg?as=1" className="bg"/>
-
-            <Route path="/" component={Signup}/>
             
             </div>
         )

f0f6e56064da81030f9da6aa9b149a43d87dddf9
github username <github email address>
2019-01-25 22:52:26 -0500

    clean up

--- frontend_react/src/components/cards/CardInfo.js
+++ frontend_react/src/components/cards/CardInfo.js
@@ -21,19 +21,6 @@ const CardInfo = (props) => {
             <h3>{capitalize(props.card.card_type)} Arcana</h3>                <p><strong>Meaning Upright:</strong> {props.card.meaning_up}</p>
             <p><strong>Meaning Reversed:</strong> {props.card.meaning_rev}</p>
             <p><strong>Description:</strong> {props.card.desc} </p>
-
-            {/* <Modal trigger={<Button>Show Modal</Button>}>
-                <Modal.Header>{props.card.name}</Modal.Header>
-
-                <Modal.Content>
-                    <Modal.Description>
-                        <Header>{capitalize(props.card.card_type)}</Header>
-                        <p>Meaning Upright: {props.card.meaning_up} </p>
-                        <p>Meaning Reversed: {props.card.meaning_rev} </p>
-                        <p>Description: {props.card.desc} </p>
-                    </Modal.Description>
-                </Modal.Content>
-            </Modal> */}
         </div>
     )
 }
